7f8a6fb5c87130da3fca3615ee74d5c377ebac3c
Eren Doru <erendoru@hotmail.com>
2020-10-14 22:34:39 +0300

    Navbar Component

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './style.scss'
+
+function Navbar() {
+    return (
+        <div className='navbar'>
+            <div className="logo">
+                <h1>Github</h1><span className='logo-right'> Jobs</span>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
