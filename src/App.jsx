c62f94482b114ddd24d6ebb14667f227eabb059e
Eren Doru <erendoru@hotmail.com>
2020-10-15 01:32:00 +0300

    Sidebar added
    I added the sidebar component to the left side.

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Navbar from './components/Navbar/Navbar'
 import SearchBar from './components/SearchBar/Search'
 import './App.scss'
 import FetchData from './components/FetchData/FetchData'
+import SideBar from './components/SideBar'
 
 const App = () => {
     return (
         <div className="App">
             <Navbar />
             <SearchBar />
+            <SideBar />
             <FetchData />
         </div>
     )
