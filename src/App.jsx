cdeb4b064938665f41f75ef75c7fd368d7ef3b5b
Eren Doru <erendoru@hotmail.com>
2020-10-31 02:02:46 +0300

    Deleted some components

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import SearchBar from './components/SearchBar/Search'
 import './App.scss'
-import FetchData from './components/FetchData/FetchData'
 import SideBar from './components/SideBar'
+import Jobs from './components/Jobs/Index'
 
 const App = () => {
     return (
         <div className="App">
             <Navbar />
             <SearchBar />
-            <SideBar />
-            <FetchData />
+            <div className='body'>
+                <SideBar />
+                <Jobs />
+            </div>
         </div>
     )
 }
